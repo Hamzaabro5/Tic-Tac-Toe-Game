8912ba28a78593263ea8a004de8e6630ed09ba6e
Hamzaabro5 <hamzaabro683@gmail.com>
2024-07-23 16:03:25 +0500

    Tic Tac Toe

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const boxes = document.querySelectorAll(`.Box`)
+const restartBtn = document.querySelector(`.restartBtn`)
 const resetBtn = document.querySelector(`.resetBtn`)
+const msgContainer = document.querySelector(`.msg-container`)
+const msg = document.querySelector(`.msg`)
 let turnX = true;
 
 const winPatterns = [
@@ -12,6 +15,7 @@ const winPatterns = [
     [0, 4, 8],
     [2, 4, 6],
 ]
+
 
 boxes.forEach((box) =>{
     box.addEventListener(`click` , ()=>{
@@ -27,16 +31,85 @@ boxes.forEach((box) =>{
     })
 })
 
+
+// Checking who is winner
+
 const checkWin = ()=>{
     for (let pattern of winPatterns) {
+        
         // console.log(pattern[0], pattern[1], pattern[2]);
+        
         let pos1Value = boxes[pattern[0]].innerText;
         let pos2Value = boxes[pattern[1]].innerText;
         let pos3Value = boxes[pattern[2]].innerText;
+        
         if (pos1Value != `` && pos2Value != `` && pos3Value != ``) {
             if(pos1Value === pos2Value && pos2Value === pos3Value){
-                console.log(`Winner` , pos1Value);
+                
+                // console.log(`Winner` , pos1Value);
+                
+                showWinner(pos1Value);
+                return true;
             }
         }
     }
 }
+
+// Checking who is winner
+
+
+
+
+// Disable  Boxes 
+
+const disableBoxes = ()=>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+// Disable Boxes 
+
+
+
+
+// Rendering Winner Of Game
+
+const showWinner = (winner) =>{
+    msg.innerHTML = `Congratulation Player${winner} Won The Game`
+    msgContainer.classList.remove(`hidden`);
+    disableBoxes();
+}
+
+// Rendering Winner Of Game
+
+
+
+// Enable boxes
+
+const enableBoxes = ()=>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerHTML = ``
+    }
+}
+
+// Enable boxes
+
+
+// Play Again Button
+
+const playAgain = ()=>{
+    turnX = true;
+    enableBoxes();
+    msgContainer.classList.add(`hidden`)
+}
+
+restartBtn.addEventListener(`click` , ()=>{
+    playAgain();
+})
+resetBtn.addEventListener(`click` , ()=>{
+    playAgain();
+})
+
+// Play Again Button
